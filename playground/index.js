fd081b6a6328307eda48c956dc46b15525d7c481
James Lal <james@lightsofapollo.com>
2012-04-26 12:36:12 -0700

    upgraded playground

--- playground/index.js
+++ playground/index.js
@@ -10,9 +10,13 @@
 
   backend.connect(function(){
     var device = window.device = new Marionette.Client(backend);
+    console.log(device);
     window.device.startSession(function(){
+      console.log('starting session');
       device.send({
-        type: 'getUrl'
+        type: 'executeScript',
+        value: 'return window.location',
+        args: []
       }, function(data){
         console.log(data.value);
       });
