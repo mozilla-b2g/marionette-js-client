33faa53d4c8d8bf826c0dd7c5048d324a086f56e
James Lal <james@lightsofapollo.com>
2012-05-02 19:37:13 -0700

    Making library more node friendly. Fixing node xhr support.

--- playground/index.js
+++ playground/index.js
@@ -5,12 +5,16 @@
   var driver = new Marionette.Drivers.HttpdPolling();
 
   driver.connect(function() {
-    var client = new Marionette.Client(driver);
-    client.startSession(function() {
-      client.goUrl('http://google.com', function(e) {
-        console.log(e);
-      });
-    });
+     var client = new Marionette.Client(driver);
+     client.startSession(function() {
+       client.goUrl('http://google.com', function(e) {
+        var el = client.findElement('input[name=\'q\']', function(el) {
+          el.sendKeys('zomg', function(){
+          
+          });
+        });
+       });
+     });
   });
 
 
