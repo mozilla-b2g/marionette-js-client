acf5acd358b9d85935b66a133b897072cb467ec4
James Lal <james@lightsofapollo.com>
2013-08-01 13:41:11 -0700

    regenerate api docs

--- api-docs/api.js
+++ api-docs/api.js
@@ -1,14 +1,14 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Marionette.Actions",
         "Marionette.Client",
         "Marionette.CommandStream",
         "Marionette.Drivers.Abstract",
-        "Marionette.Drivers.Httpd",
         "Marionette.Drivers.MozTcp",
         "Marionette.Drivers.Tcp",
         "Marionette.Element",
-        "Marionette.Xhr"
+        "Marionette.MultiActions"
     ],
     "modules": [],
     "allModules": []
