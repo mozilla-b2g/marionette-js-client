170b9363246524394c1679bb6ebf654ab569ace7
gaye <gaye@mozilla.com>
2015-04-10 12:20:21 -0400

    Update mozilla-download dep to v1.0

--- test/helper.js
+++ test/helper.js
@@ -1,64 +1,60 @@
-(function() {
+var assert = require('chai').assert;
 
-  global.assert = require('chai').assert;
+var helper = global.helper = {};
+global.assert = assert;
 
-  global.helper = {
-    requireLib: function(path, cb) {
-      return require('../lib/' + path);
-    },
+helper.requireLib = function(path) {
+  return require('../lib/' + path);
+};
 
-    nsFind: function(obj, string) {
-      var result = obj,
-          part,
-          parts = string.split('.');
-
-      while ((part = parts.shift())) {
-        if (result[part]) {
-          result = result[part];
-        } else {
-          throw new Error('Cannot find ' + string + ' in object ');
-        }
-      }
-      return result;
-    },
-
-    require: function(path, component, cb) {
-
-      if (typeof(component) === 'function') {
-        //new module pattern
-        cb = component;
-        component = path;
-
-        if (/^support/.test(path)) {
-          path = '/test/' + path;
-        } else {
-          path = '/lib/marionette/' + path;
-        }
-      } else {
-        //old system
-        path = '/lib/' + path;
-      }
-
-      cb(require('..' + path));
+helper.nsFind = function(obj, string) {
+  var result = obj;
+  var parts = string.split('.');
+  var part;
+  while (part = parts.shift()) {
+    if (!result[part]) {
+      throw new Error('Cannot find ' + string + ' in object ');
     }
-  };
 
-  helper.require('responder', function(obj) {});
+    result = result[part];
+  }
 
-  helper.require('example-commands', function(obj) {
-    global.exampleCmds = obj;
-  });
+  return result;
+};
 
-  helper.require('support/device-interaction', function(obj) {
-    global.DeviceInteraction = obj;
-  });
+helper.require = function(path, component, cb) {
+  if (typeof(component) === 'function') {
+    //new module pattern
+    cb = component;
+    component = path;
 
-  helper.require('support/socket', function(obj) {
-    global.FakeSocket = obj;
-  });
+    if (/^support/.test(path)) {
+      path = '/test/' + path;
+    } else {
+      path = '/lib/marionette/' + path;
+    }
+  } else {
+    //old system
+    path = '/lib/' + path;
+  }
 
-  helper.require('support/mock-driver', function(obj) {
-    global.MockDriver = obj;
-  });
+  cb(require('..' + path));
+};
 
-}.call(this));
+helper.require('responder', function(obj) {});
+
+helper.require('example-commands', function(obj) {
+  this.exampleCmds = obj;
+});
+
+helper.require('support/device-interaction', function(obj) {
+  this.DeviceInteraction = obj;
+});
+
+helper.require('support/socket', function(obj) {
+  this.FakeSocket = obj;
+});
+
+helper.require('support/mock-driver', function(obj) {
+  this.MockDriver = obj;
+});
