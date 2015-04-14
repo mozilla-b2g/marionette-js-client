cdf75d3470332dac30897d9f16cf5c01ae7b3332
gaye <gaye@mozilla.com>
2015-04-14 14:04:11 -0400

    regenerate api docs

---
+++ api-docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Marionette.Actions",
+        "Marionette.Client",
+        "Marionette.CommandStream",
+        "Marionette.Drivers.Abstract",
+        "Marionette.Drivers.MozTcp",
+        "Marionette.Drivers.Tcp",
+        "Marionette.Element",
+        "Marionette.MultiActions"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
